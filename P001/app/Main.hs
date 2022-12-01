e64a230e6e9d39f9c90041324c0e68528199c435
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-12-01 23:00:12 +0200

    refactor

--- P001/app/Main.hs
+++ P001/app/Main.hs
@@ -4,5 +4,5 @@ import Lib
 main :: IO ()
 main = do
   str <- readFile "input"
-  putStrLn $ show (solve 1 str)
-  putStrLn $ show (solve 3 str)
+  print $ solve 1 str
+  print $ solve 3 str
--- P001/src/Lib.hs
+++ P001/src/Lib.hs
@@ -6,10 +6,10 @@ import Text.Parsec
 import Text.Parsec.String ( Parser )
 
 elf :: Parser Integer
-elf = some (read <$> some digit <* char '\n') >>= pure . sum
+elf = sum <$> some (read <$> some digit <* char '\n')
 
 elves :: Parser [Integer]
 elves = sepBy1 elf (char '\n')
 
 solve :: Int -> String -> Either ParseError Integer
-solve n s = sum . (take n) . reverse . sort <$> parse elves "" s
+solve n s = sum . take n . reverse . sort <$> parse elves "" s
