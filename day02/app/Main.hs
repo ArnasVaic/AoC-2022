eadd070a1a50c5bdf23944992d9660aa3fc9ba59
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-12-02 19:24:27 +0200

    day 2

---
+++ day02/app/Main.hs
@@ -0,0 +1,8 @@
+module Main (main) where
+import Lib
+
+main :: IO ()
+main = do
+  str <- readFile "input"
+  print $ solve str line -- 1st part
+  print $ solve str line' -- 2st part
---
+++ day02/src/Lib.hs
@@ -0,0 +1,62 @@
+module Lib where
+
+import Data.List
+import Control.Applicative ( some )
+import Text.Parsec
+import Text.Parsec.String ( Parser )
+
+data Hand = Rock | Paper | Scissors deriving (Eq, Show)
+
+win :: Hand -> Hand
+win Rock = Scissors
+win Paper = Rock
+win Scissors = Paper
+
+lose :: Hand -> Hand
+lose Rock = Paper
+lose Paper = Scissors
+lose Scissors = Rock
+
+-- Rock Paper Scissors Rock Scissors
+handScore :: Hand -> Int
+handScore Rock = 1
+handScore Paper = 2
+handScore Scissors = 3
+
+winScore :: Hand -> Hand -> Int
+winScore x y
+  | y == win x  = 6
+  | y == lose x = 0
+  | otherwise   = 3
+
+score :: Hand -> Hand -> Int
+score x y = handScore x + winScore x y
+
+hand :: Parser Hand
+hand = 
+      Rock      <$ oneOf "AX"
+  <|> Paper     <$ oneOf "BY"
+  <|> Scissors  <$ oneOf "CZ"
+
+line :: Parser Int
+line = do
+  a <- hand <* char ' '
+  b <- hand <* char '\n'
+  pure $ score b a
+
+-- part 1
+solve :: String -> Parser Int -> Either ParseError Int
+solve s p = parse (sum <$> some p) "" s
+
+choose :: Char -> Hand -> Hand
+choose 'X' = win
+choose 'Y' = id
+choose 'Z' = lose
+choose _ = undefined
+
+-- part 2
+line' :: Parser Int
+line' = do
+  a <- hand <* char ' '
+  c <- oneOf "XYZ" <* char '\n'
+  pure $ score (choose c a) a
