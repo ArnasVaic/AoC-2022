4ad6041df00465b11cc1cbf288910200f4ce6044
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-12-04 13:10:11 +0200

    day 04 part 2

--- day04/app/Main.hs
+++ day04/app/Main.hs
@@ -4,5 +4,5 @@ import Lib
 main :: IO ()
 main = do
   str <- readFile "input"
-  print $ solve str  -- 1st part
-  print $ solve str  -- 2nd part
+  print $ solve str pair  -- 1st part
+  print $ solve str pair' -- 2nd part
--- day04/src/Lib.hs
+++ day04/src/Lib.hs
@@ -18,5 +18,13 @@ pair = do
   b1 <- num <* char '\n'
   pure $ (a0 >= b0 && a1 <= b1) || (b0 >= a0 && b1 <= a1)
 
-solve :: String -> Either ParseError Int
-solve = parse (length . filter (== True) <$> some pair) ""
+pair' :: Parser Bool
+pair' = do
+  a0 <- num <* char '-'
+  a1 <- num <* char ','
+  b0 <- num <* char '-'
+  b1 <- num <* char '\n'
+  pure $ not $ a1 < b0 || b1 < a0
+
+solve :: String -> Parser Bool -> Either ParseError Int
+solve s p = parse (length . filter (== True) <$> some p) "" s
